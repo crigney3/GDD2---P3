e99a96601d13a1591524bd6fb5937a9492aece5f
John J Hayes <34247425+ChefKef@users.noreply.github.com>
2020-11-11 11:06:21 -0500

    Created Potion Mixing Screen

---
+++ Assets/Scripts/CauldronManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class CauldronManager : MonoBehaviour
+{
+    public GameObject[] ingredients;
+
+    private bool filled;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ingredients = new GameObject[8]; //Can only have a max of 8 ingredients.
+        filled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void EmptyCauldron()
+    {
+        ingredients = new GameObject[8]; //Just refresh the cauldron.
+    }
+
+    public void AddIngredient(GameObject ingredient)
+    {
+        for(int a = 0; a < 8; a++)
+        {
+            if(ingredients[a] == null) //Add ingredient to first empty slot in list.
+            {
+                ingredients[a] = ingredient;
+                if(a == 7) //Check to see if cauldron is full.
+                {
+                    filled = true;
+                }
+            }
+        }
+    }
+}
