e5ab0b33297b40a4b357fa09f73d78a9e8c3f49d
Triton Adamski <txa7348@rit.edu>
2020-11-16 12:00:15 -0500

    Fixed Potion Encyclopedia in Encyclopedia Test Scene.

--- Assets/Scripts/EncyclopediaScript.cs
+++ Assets/Scripts/EncyclopediaScript.cs
@@ -12,12 +12,12 @@ public class EncyclopediaScript : MonoBehaviour
     void Start()
     {
         SetPageActive(0);
-        for(int i = 0; i < buttonList.Count; i++)
+        /*for(int i = 0; i < buttonList.Count; i++)
         {
             buttonList[i].onClick.AddListener(() => SetPageActive(i));
             buttonList[i].onClick.AddListener(ButtonDebugText);
             buttonList[i].onClick.AddListener(delegate { ButtonDebugText(i); });
-        }
+        }*/
     }
 
     // Update is called once per frame
@@ -34,11 +34,46 @@ public class EncyclopediaScript : MonoBehaviour
         }
     }
 
-    void SetPageActive(int page)
+    public void SetPageActive(int page)
     {
         HideAllPages();
         pages[page].SetActive(true);
     }
+
+    public void SetPrevPageActive()
+    {
+        int activePage = -1;
+        for (int i = 0; i < pages.Count; i++)
+        {
+            if (pages[i].activeInHierarchy)
+            {
+                activePage = i;
+                break;
+            }
+        }
+        Debug.Log("Active: " + activePage);
+
+        if (activePage == 0) //Active page is last page
+            SetPageActive(pages.Count - 1);
+        else if (activePage >= 0) //Active page is not last page
+            SetPageActive(activePage - 1);
+    }
+
+    public void SetNextPageActive()
+    {
+        int activePage = -1;
+        for (int i = 0; i < pages.Count; i++)
+        {
+            if (pages[i].activeInHierarchy)
+                activePage = i;
+        }
+
+        if (activePage == pages.Count - 1) //Active page is last page
+            SetPageActive(0);
+        else if (activePage >= 0) //Active page is not last page
+            SetPageActive(activePage + 1);
+    }
+
 
     void ButtonDebugText()
     {
