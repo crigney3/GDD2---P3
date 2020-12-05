3a25c8527708a48a9230aad6132042c91e187bcc
Triton Adamski <txa7348@rit.edu>
2020-12-05 12:15:51 -0500

    Fixed typo in Controls scene. Prevented potion and caudlron buttons from showing in other states. Allowed player to empty cauldron at any time.

--- Assets/Scripts/CauldronManager.cs
+++ Assets/Scripts/CauldronManager.cs
@@ -6,16 +6,23 @@ using UnityEngine;
 public class CauldronManager : MonoBehaviour
 {
     public GameObject[] ingredients;
-    public Sprite noHover;
-    public Sprite hover;
+    public Sprite noHoverEmpty;
+    public Sprite hoverEmpty;
+    public Sprite noHoverMid;
+    public Sprite hoverMid;
+    public Sprite noHoverFull;
+    public Sprite hoverFull;
 
     private bool filled;
     private GameObject potion;
     static GameManager gm;
     public GameObject audioManager;
     private AudioManager audioScript;
-    public GameObject potionBtns;
+    
+    public GameObject cauldronBtns;
+    public GameObject fillBtn;
     public GameObject ingredientPage;
+    private bool hasIngredients;
 
     public bool Filled
     {
@@ -27,6 +34,7 @@ public class CauldronManager : MonoBehaviour
     {
         ingredients = new GameObject[8]; //Can only have a max of 8 ingredients.
         filled = false;
+        hasIngredients = false;
         potion = GameObject.Find("Potion");
         gm = GameManager.Instance;
         audioScript = audioManager.GetComponent<AudioManager>();
@@ -36,24 +44,42 @@ public class CauldronManager : MonoBehaviour
     void Update()
     {
         //if(filled)
-            //FillPotion();
+        //FillPotion();
 
-        if (gm.BlockButtons && GetComponent<SpriteRenderer>().sprite != noHover)
-            GetComponent<SpriteRenderer>().sprite = noHover;
+        if (gm.BlockButtons)
+        {
+            if (filled && GetComponent<SpriteRenderer>().sprite != noHoverFull)
+                GetComponent<SpriteRenderer>().sprite = noHoverFull;
+            else if (!filled && hasIngredients && GetComponent<SpriteRenderer>().sprite != noHoverMid)
+                GetComponent<SpriteRenderer>().sprite = noHoverMid;
+            else if (!filled && !hasIngredients && GetComponent<SpriteRenderer>().sprite != noHoverEmpty)
+                GetComponent<SpriteRenderer>().sprite = noHoverEmpty;
+        }
 
-        if (filled && !potionBtns.activeInHierarchy) //Shows buttons when potion has been made
-            potionBtns.SetActive(true);
-        else if (!filled && potionBtns.activeInHierarchy) //Hides buttons when potion has not been made
-            potionBtns.SetActive(false);
+        if (filled && GetComponent<SpriteRenderer>().sprite != noHoverFull && GetComponent<SpriteRenderer>().sprite != hoverFull)
+            GetComponent<SpriteRenderer>().sprite = noHoverFull;
+        else if (!filled && hasIngredients && GetComponent<SpriteRenderer>().sprite != noHoverFull && GetComponent<SpriteRenderer>().sprite != hoverMid)
+            GetComponent<SpriteRenderer>().sprite = noHoverMid;
+        else if (!filled && !hasIngredients && GetComponent<SpriteRenderer>().sprite != noHoverEmpty && GetComponent<SpriteRenderer>().sprite != hoverEmpty)
+            GetComponent<SpriteRenderer>().sprite = noHoverEmpty;
 
-        if (gm.currentState == GameManager.State.Clear || gm.currentState == GameManager.State.Fail) //Empties the cauldron after finishing the level
-            EmptyCauldron();
+
+        if (hasIngredients && gm.currentState == GameManager.State.Brewing && !cauldronBtns.activeInHierarchy) //Shows buttons when ingredients have been added
+            cauldronBtns.SetActive(true);
+        else if ((!hasIngredients || gm.currentState != GameManager.State.Brewing) && cauldronBtns.activeInHierarchy) //Hides buttons when ingredients have not been added or when in a different state
+            cauldronBtns.SetActive(false);
+
+        if (filled && cauldronBtns.activeInHierarchy) //Shows button when cauldron is full
+            fillBtn.SetActive(true);
+        else if (!filled || !cauldronBtns.activeInHierarchy) //Hides button when cauldron is not full
+            fillBtn.SetActive(false);
     }
 
     public void EmptyCauldron()
     {
         ingredients = new GameObject[8]; //Just refresh the cauldron.
         filled = false;
+        hasIngredients = false;
         ingredientPage.GetComponent<IngredientPage>().Restart();
     }
 
@@ -63,33 +89,48 @@ public class CauldronManager : MonoBehaviour
         {
             if(ingredients[a] == null) //Add ingredient to first empty slot in list.
             {
+                if (!hasIngredients)
+                    hasIngredients = true;
                 audioScript.PlayFX(9);
                 ingredients[a] = ingredient;
                 if(a == 7) //Check to see if cauldron is full.
                 {
                     filled = true;
-                    FillPotion();
+                    //FillPotion();
                 }
                 break; //Exit for loop when ingredient is added.
             }
         }
     }
 
-    private void FillPotion()
+    public void FillPotion()
     {
-        audioScript.PlayFX(11);
         potion.GetComponent<PotionManager>().fillPotion(ingredients);
     }
 
     private void OnMouseEnter()
     {
         if (!gm.BlockButtons)
-            GetComponent<SpriteRenderer>().sprite = hover;
+        {
+            if (filled)
+                GetComponent<SpriteRenderer>().sprite = hoverFull;
+            else if (hasIngredients)
+                GetComponent<SpriteRenderer>().sprite = hoverMid;
+            else
+                GetComponent<SpriteRenderer>().sprite = hoverEmpty;
+        }
     }
 
     private void OnMouseExit()
     {
         if (!gm.BlockButtons)
-            GetComponent<SpriteRenderer>().sprite = noHover;
+        {
+            if (filled)
+                GetComponent<SpriteRenderer>().sprite = noHoverFull;
+            else if (hasIngredients)
+                GetComponent<SpriteRenderer>().sprite = noHoverMid;
+            else
+                GetComponent<SpriteRenderer>().sprite = noHoverEmpty;
+        }
     }
 }
